36d1e95ac772592f54eb267bf453ce1691855d80
Blaedj <blaedj@gmail.com>
2013-09-14 17:40:37 -0500

    add endianness testing, skeleton out host/ip resolving

--- libcs5651/Proj1/main.cpp
+++ libcs5651/Proj1/main.cpp
@@ -10,25 +10,49 @@
 
 using namespace bcjnetworks;
 using namespace sivelab;
+using namespace std;
 
 
-int main(int argc, char *argv[]) {
+bool isLittleEndian();
+
+
+int main( int argc, char *argv[] ) {
   NetworkArgs args;
   args.process( argc, argv );
   AddressNameMapper identifier;
 
   if (args.hasHostname) {
-    //identifier = new AddressNameMapper( args.hostname );
-    std::string resolved_ip_addr = identifier;
-    std::cout << "has a hostname!\n";
+
+     string resolved_addr = identifier.getIPAddr( args.hostname );
+    cout << "IP address is " << resolved_addr << "\n";
 
   } else if (args.hasHostIP) {
-    //    identifier = new AddressNameMapper( args.hostIP );
-    std::cout << "has an ipaddress!\n";
-  } //else  return 0;
+    string resolved_hostname = identifier.getHostname( args.hostIP );
+    cout << "hostname is: " << resolved_hostname << "\n";
+  }
 
-  std::cout << "this is a place holder for the Endian-ness\n";
-  std::cout << "this is a placeholder for the hostname or IP address\n";
-
+  if(isLittleEndian()) {
+    cout << "Byte order is Little Endian\n";
+  } else {
+    cout << "Byte order is Big Endian\n";
+  }
   return 0;
+}
+
+
+/* tests the byte-order of this machine.
+ * returns true if little endian, false if big endian.
+ */
+bool isLittleEndian() {
+  // we cast an unsigned int (1) to a char
+  // if the zeroth index of the char is 1,
+  // then the least significant byte is
+  // at starting address, so must be Little Endian
+  unsigned int i = 1;
+  char *char_pointer = (char *) &i;
+  if (char_pointer[0] == 1) {
+    return true;
+  } else {
+    return false;
+  }
 }
