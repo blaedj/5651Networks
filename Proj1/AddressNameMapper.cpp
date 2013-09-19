fb98bc0821aead23bcabd676593ace1a44be9555
Blaed <blaedj@gmail.com>
2013-09-19 07:47:16 -0500

    start getaddrinfo call

--- Proj1/AddressNameMapper.cpp
+++ Proj1/AddressNameMapper.cpp
@@ -5,7 +5,13 @@
 
 #include <iostream>
 #include <string>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
 #include "AddressNameMapper.h"
+
+
 
 using namespace bcjnetworks;
 using namespace std;
@@ -17,9 +23,21 @@ AddressNameMapper::AddressNameMapper()
 AddressNameMapper::~AddressNameMapper() {}
 
 string AddressNameMapper::getHostname(string address){
-  return "a test local hostname";
+return "a test local hostname";
 }
 
 string AddressNameMapper::getIPAddr( string ip_address) {
+
+  struct addrinfo address_hints;
+  struct addrinfo *results, *rp;
+  int sfd, addressinfo_result_code;
+  struct sockaddr_storage peer_address;
+  socklen_t peer_address_length;
+  ssize_t nread;
+  char buffer[BUFFER_SIZE];
+
+  getaddrinfo();
+
+
   return "127.0.0.1";
 }
