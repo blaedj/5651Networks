f5dfc52f7b961c85d6078f26e12492edbd2d4104
Blaed <blaedj@gmail.com>
2013-09-21 11:26:56 -0500

    updated slides, fix formatting in AddressNameMapper.cpp

--- Proj1/AddressNameMapper.cpp
+++ Proj1/AddressNameMapper.cpp
@@ -31,15 +31,12 @@ string AddressNameMapper::getHostname(string ip_address){
   return "a test local hostname";
 }
 
- string AddressNameMapper::getIPAddr( string hostname) {
-
-  // struct addrinfo address_hints;
-   struct addrinfo *results;//, *result_iter;
+string AddressNameMapper::getIPAddr( string hostname) {
+  struct addrinfo *results;//, *result_iter;
   int result_code;
   char buffer[BUFFER_SIZE];
-  // make sure address_hints is cleared out in mem
+  // struct addrinfo address_hints;
   // memset( &address_hints, 0, sizeof(struct addrinfo) );
-
   // address_hints.ai_flags = 0; // flags are optional
   // address_hints.ai_family = AF_UNSPEC; // don't specify IPv4 or IPv6
   // address_hints.ai_socktype = 0; // any socket type
@@ -48,22 +45,20 @@ string AddressNameMapper::getHostname(string ip_address){
   // address_hints.ai_addr = NULL;
   // address_hints.ai_next = NULL;
 
-  // the hostname converted to a c_str, 0 for service,
+  // the hostname converted to a c_str, 0 for service, 0 for hints
   result_code = getaddrinfo( hostname.c_str(), 0, 0, &results );
 
   if(result_code != 0){
-    // TODO: Create an exception for this
     cerr << "this is a placeholder for an exception, couldn't get addrinfo.\n";
     exit(EXIT_FAILURE);
   }
-
   char ip_string[INET_ADDRSTRLEN];
-    // get the address information struct
-    struct sockaddr_in *result_addr =(struct sockaddr_in *)(results->ai_addr);
+  // get the address information struct
+  struct sockaddr_in *result_addr =(struct sockaddr_in *)(results->ai_addr);
 
-    // convert the binary ip address in resultAddr to dotted decimal form
-    inet_ntop(results->ai_family, (const void *)&(result_addr->sin_addr),
-	      ip_string, INET_ADDRSTRLEN);
+  // convert the binary ip address in resultAddr to dotted decimal form
+  inet_ntop(results->ai_family, (const void *)&(result_addr->sin_addr),
+	    ip_string, INET_ADDRSTRLEN);
 
   // release memory
   freeaddrinfo(results);
