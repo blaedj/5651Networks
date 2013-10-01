bdd9cd9122146b8ad01635d9d71c1ef1e449601a
Blaed <blaedj@gmail.com>
2013-10-01 08:36:41 -0500

    add simple socket constructor

--- libcppSocket/Socket.cpp
+++ libcppSocket/Socket.cpp
@@ -9,6 +9,7 @@
 #include <sys/socket.h>
 
 #include "Socket.h"
+#include "SocketException.h"
 
 using namespace libcppsocket;
 using namespace std;
@@ -19,6 +20,7 @@ Socket::Socket(){}
 Socket::Socket(int port ){
   int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
   if ( socket_fd < 0 ) {
-
+    throw SocketException("Unable to create socket");
   }
+
 }
