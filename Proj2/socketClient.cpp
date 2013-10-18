6854c6eed71e3249c36c458a6a5bab30286a7c94
Blaedj <blaedj@gmail.com>
2013-10-18 10:51:39 -0500

    final touches

--- Proj2/socketClient.cpp
+++ Proj2/socketClient.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <sstream>
 #include <fstream>
+#include <vector>
 
 #include "libcppSocket/libcppsocket/ClientSocket.h"
 #include "libcs5651/CS5651Lib/handleNetworkArgs.h"
@@ -14,32 +15,65 @@ using namespace libcppsocket;
 using namespace std;
 using namespace sivelab;
 
+vector<string> getFiles();
+void error_check(char * data);
+
 int main( int argc, char *argv[] ) {
+  // TODO: extract region setup
   NetworkArgs args;
   args.process( argc, argv );
   if(!args.hasHostIP) {
-    cout << "A hostIP address and port number are required\n";
+    cout << "hostIP address and port number are required\n";
     exit(0);
   }
   string hostIP = args.hostIP;
   int port = args.port;
   ClientSocket socket(hostIP, port);
+  // end setup
 
+  cout << "Welcome to this simple client!\n Note that any files recieved from "
+       << "a server will be placed in the 'received/' directory in the "
+       << "current directory.\n If no such directory exists, one will be "
+       << "created for you.\n\n";
+
+  vector<string> request_files = getFiles();
   MessageBuffer request;
-  char req_msg[] = "test.txt";
-  request.add(&req_msg[0], sizeof(req_msg));
-
-  socket.send_request(request);
-
   MessageBuffer response;
-  response = socket.get_response();
-  int write_size = response.size();
+  for(size_t i =0; i < request_files.size(); i++) {
+    string req_msg = request_files[i];
+    request.add((char*)req_msg.c_str(), sizeof(req_msg));
+    socket.send_request(request);
 
-  ofstream data_writer("testRecvd.txt", ios::binary | ios::out);
-  data_writer.write(response.get_data(), write_size);
-  data_writer.close();
-
+    string file_name = "received/" + request_files[i];
+    response = socket.get_response();
+    if(response.size() < 100){
+      error_check(response.get_data());
+    } else {
+      int write_size = response.size();
+      ofstream data_writer(file_name.c_str(), ios::binary | ios::out);
+      data_writer.write(response.get_data(), write_size);
+      data_writer.close();
+      cout << file_name << " was written\n";
+    }
+  }
   socket.close_socket();
-
   return 0;
+}
+
+vector<string> getFiles(){
+  vector<string> files;
+  string aFile;
+  while(aFile != "q") {
+    cout << "enter a file name to request, 'q' when finished.\n";
+    cin >> aFile;
+    if(aFile != "q"){files.push_back(aFile);}
+  }
+  return files;
+}
+
+void error_check(char * data){
+  string err_msg(data);
+    cout << "Sorry, the server returned the following error: \n"
+	 << err_msg<< "\n";
+  exit(0);
 }
--- Proj2/socketServer.cpp
+++ Proj2/socketServer.cpp
@@ -14,34 +14,52 @@ using namespace libcppsocket;
 using namespace std;
 
 void signal_handler();
+bool file_exists(char * filename);
 
 int main( int argc, char *argv[] ) {
-  int port = 45002;
+  ///int port = 45002;
+  sivelab::NetworkArgs args;
+  args.process(argc, argv);
+  if(args.port < 1024){
+    cout << "Please enter a valid port number with the option: '-p PORT_#'\n";
+    exit(0);
+  }
+  int port = args.port;
   cout << "Starting server on port "<<port<<"...\n";
   ServerSocket server(port);
   server.open_for_clients();
   cout <<"Server up and running\n";
+  cout << "Please note that this server is only able to successfully process"
+       << " and send one file per client request\n";
   while(true){
     try{
       int client_fd = server.accept_connection();
       MessageBuffer data_recieved = server.recieve(client_fd);
       cout << "File requested: '"<< data_recieved.get_data() <<"'\n";
       char * file_requested = data_recieved.get_data();
-      ifstream reader(file_requested, ios::in|ios::binary|ios::ate);
-      ifstream::pos_type file_size;
-      char * data_buf;
-      if (reader.is_open()) {
-	file_size = reader.tellg();
-	data_buf= new char [file_size];
-	reader.seekg (0, ios::beg);
-	reader.read (data_buf, file_size);
-	reader.close();
+      if(!file_exists(file_requested)){
+	MessageBuffer fail_msg;
+	string _404 = "404 FILE NOT FOUND";
+	fail_msg.add((char *)_404.c_str(),sizeof(_404.c_str()));
+	server.respond(fail_msg, client_fd);
+	cout << _404 << "\n";
+      } else {
+	ifstream reader(file_requested, ios::in|ios::binary|ios::ate);
+	ifstream::pos_type file_size;
+	char * data_buf;
+	if (reader.is_open()) {
+	  file_size = reader.tellg();
+	  data_buf= new char [file_size];
+	  reader.seekg (0, ios::beg);
+	  reader.read (data_buf, file_size);
+	  reader.close();
+	}
+	MessageBuffer response_msg;
+	response_msg.add(data_buf, file_size);
+	delete[] data_buf;
+	server.respond(response_msg, client_fd);
+	cout <<"'" <<file_requested << "'"<< " was sent.\n";
       }
-      MessageBuffer response_msg;
-      response_msg.add(data_buf, file_size);
-      delete[] data_buf;
-      server.respond(response_msg, client_fd);
-      cout <<"'" <<file_requested << "'"<< " was sent.\n";
     } catch( SocketException e) {
       string str(e.what());
       cout << "an error occured: '"<< str << "'\n";
@@ -49,4 +67,9 @@ int main( int argc, char *argv[] ) {
     }
   }
   return 0;
+}
+
+bool file_exists(char * filename){
+  ifstream file(filename);
+  return file;
 }
